e8be7b9fba3f79a681a5a00e8a591f8a93ddfab4
Gabriel G <base@gbr.pw>
2020-04-19 22:22:15 -0500

    Terreno del planeta aislado

--- src/app/views/planets/planet/planet.component.ts
+++ src/app/views/planets/planet/planet.component.ts
@@ -1,10 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-planet',
   templateUrl: './planet.component.html',
   styleUrls: ['./planet.component.scss']
 })
-export class PlanetComponent {
+export class PlanetComponent implements OnInit{
   @Input() name: string;
+  @Input() terrain: string;
+
+  public firstTerrain;
+
+  ngOnInit() {
+    this.firstTerrain = this.terrain.split(',');
+    this.firstTerrain = this.firstTerrain[0].split(' ').join('');
+  }
 }
