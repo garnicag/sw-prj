1b544452de7f457d02c379fa0abf50999194d5fc
Gabriel G <base@gbr.pw>
2020-05-05 21:05:09 -0500

    Actualización  personajes

--- src/app/views/characters/characters.component.ts
+++ src/app/views/characters/characters.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SearchService } from '../../services/search.service';
 import { GetService } from '../../services/get.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-characters',
@@ -12,6 +13,11 @@ export class CharactersComponent implements OnInit {
   public charactersList;
   public hasResults: boolean;
   public selectedCharacter;
+  public selectedCharacterHomeworld: Array<any> = [];
+  public selectedCharacterSpecies: Array<any> = [];
+  public selectedCharacterStarships: Array<any> = [];
+  public selectedCharacterVehicles: Array<any> = [];
+  public selectedCharacterFilms: Array<any> = [];
   public id;
 
   constructor(
@@ -42,12 +48,93 @@ export class CharactersComponent implements OnInit {
     );
   }
 
+  // homeworld
+  // species
+  // starships
+  // vehicles
+  // films
+
   selectCharacter(character): void {
+    this.selectedCharacterHomeworld = [];
+    this.selectedCharacterSpecies = [];
+    this.selectedCharacterStarships = [];
+    this.selectedCharacterVehicles = [];
+    this.selectedCharacterFilms = [];
+
     this.selectedCharacter = this.charactersList.filter((el) => {
       if (el.name.toLowerCase() === character.toLowerCase()) { return true; }
     });
     this.selectedCharacter = this.selectedCharacter[0];
-    console.log(this.selectedCharacter);
+
+    if (this.selectedCharacter.homeworld) {
+      this.getHomeworld(this.selectedCharacter.homeworld);
+    }
+
+    this.selectedCharacter.species.forEach(element => {
+      const id = element.split('/');
+      this.getSpecies(id[id.length - 2]);
+    });
+
+    this.selectedCharacter.vehicles.forEach(element => {
+      const id = element.split('/');
+      this.getVehicles(id[id.length - 2]);
+    });
+
+    this.selectedCharacter.starships.forEach(element => {
+      const id = element.split('/');
+      this.getStarships(id[id.length - 2]);
+    });
+
+    this.selectedCharacter.films.forEach(element => {
+      const id = element.split('/');
+      this.getFilms(id[id.length - 2]);
+    });
+
+    console.log(this.selectedCharacter, this.id);
+  }
+
+  getHomeworld(url): void {
+    let id = url.split('/');
+    id = id[id.length - 2];
+
+    this.searchService.getDetails('planets', id).subscribe(
+      result => {
+        console.log('======>>>>', result.name);
+        this.selectedCharacterHomeworld = result.name;
+      }
+    );
+  }
+
+  getSpecies(id): void {
+    this.searchService.getDetails('species', id).subscribe(
+      result => {
+        this.selectedCharacterSpecies.push(result);
+      }
+    );
+  }
+
+  getVehicles(id): void {
+    this.searchService.getDetails('vehicles', id).subscribe(
+      result => {
+        this.selectedCharacterVehicles.push(result);
+      }
+    );
+  }
+
+  getStarships(id): void {
+    this.searchService.getDetails('starships', id).subscribe(
+      result => {
+        this.selectedCharacterStarships.push(result);
+      }
+    );
+  }
+
+  getFilms(id): void {
+    this.searchService.getDetails('films', id).subscribe(
+      result => {
+        this.selectedCharacterFilms.push(result);
+      }
+    );
   }
 
   characterID(url): number {
